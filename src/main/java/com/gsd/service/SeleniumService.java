079053fcb1abafa0bad1fc34a7f88eae461b5d2f
Jeewaka Manohara <jamesheller.jeewake@gmail.com>
2023-02-22 00:22:30 +0530

    get job data

--- src/main/java/com/gsd/service/SeleniumService.java
+++ src/main/java/com/gsd/service/SeleniumService.java
@@ -1,15 +1,25 @@
 package com.gsd.service;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.TimeoutException;
 
 import com.gsd.helpers.SeleniumConfiguration;
+import com.gsd.models.Job;
 
 public class SeleniumService {
+    private static Logger logger = LoggerFactory.getLogger(SeleniumService.class);
+
     public void getJobsListings() {
+        // TODO : save all the xpath per site
+        // TODO : load the xpath from site name
         int pageNum = 1;
         int currentPage = 1;
         int maxPage = 0;
@@ -17,10 +27,31 @@ public class SeleniumService {
         String url = "https://xpress.jobs/jobs?page=" + pageNum + "&KeyWord=" + jobTitle;
         ChromeDriver chromeDriver = SeleniumConfiguration.getChromeDriver();
         chromeDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        chromeDriver.navigate().to(url);
-        WebElement jobListingsElement = chromeDriver
-                .findElement(By.xpath("//ul[@class='job_listings']"));
-        jobListingsElement.findElements(By.className("job_listing"))
-                .forEach(job -> System.out.println(job.findElement(By.className("job_listing-title")).getText()));
+        try {
+            chromeDriver.navigate().to(url);
+            WebElement jobListingsElement = chromeDriver
+                    .findElement(By.xpath("//ul[@class='job_listings']"));
+            List<WebElement> jobListings = jobListingsElement.findElements(By.className("job_listing"));
+            for (WebElement jobListing : jobListings) {
+                Job newJob = new Job();
+                newJob.title(jobListing.findElement(By.className("job_listing-title")).getText());
+                newJob.companyLogo(
+                        jobListing.findElement(By.xpath("//img[@class='company_logo']")).getAttribute("src"));
+                newJob.companyName(
+                        jobListing.findElement(By.xpath("//div[@class='job_listing-company']//strong")).getText());
+                newJob.location(
+                        jobListing.findElement(By.xpath("//div[@class='location col-md-5 col-lg-4']")).getText());
+                newJob.discription(jobListing
+                        .findElement(By.xpath("//div[@class='job_listing-overview job_listing__column']")).getText());
+                newJob.employeementPeriod(
+                        jobListing.findElement(By.xpath("//li[@class='job-type full-time']")).getText());
+                newJob.expireDate(jobListing.findElement(By.xpath("//li[@class='date']")).getText());
+                logger.info(newJob.toString());
+            }
+        } catch (NoSuchElementException | TimeoutException e) {
+            logger.error(e.getMessage());
+        } finally {
+            chromeDriver.quit();
+        }
     }
 }
